2eed6d580174f8f349aba4cd48ff691e2c48000c
Ernst-D <ernst.dzerevanko@gmail.com>
2025-07-06 16:40:38 +0400

    migrate more serializers to kt

--- playwright/src/main/java/com/microsoft/playwright/impl/serialization/StringMapSerializer.kt
+++ playwright/src/main/java/com/microsoft/playwright/impl/serialization/StringMapSerializer.kt
@@ -1,21 +1,22 @@
- package com.microsoft.playwright.impl.serialization;
+package com.microsoft.playwright.impl.serialization
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
-import com.microsoft.playwright.PlaywrightException;
+import com.google.gson.JsonElement
+import com.google.gson.JsonSerializationContext
+import com.google.gson.JsonSerializer
+import com.microsoft.playwright.PlaywrightException
+import com.microsoft.playwright.impl.Serialization
+import java.lang.reflect.Type
 
-import java.lang.reflect.Type;
-import java.util.Map;
-
-import static com.microsoft.playwright.impl.Serialization.toProtocol;
-
-public class StringMapSerializer implements JsonSerializer<Map<String, String>> {
-  @Override
-  public JsonElement serialize(Map<String, String> src, Type typeOfSrc, JsonSerializationContext context) {
-    if (!"java.util.Map<java.lang.String, java.lang.String>".equals(typeOfSrc.getTypeName())) {
-      throw new PlaywrightException("Unexpected map type: " + typeOfSrc);
+class StringMapSerializer : JsonSerializer<MutableMap<String?, String?>?>
+{
+    override fun serialize(
+        src: MutableMap<String?, String?>?, typeOfSrc: Type, context: JsonSerializationContext?
+    ): JsonElement
+    {
+        if ("java.util.Map<java.lang.String, java.lang.String>" != typeOfSrc.getTypeName())
+        {
+            throw PlaywrightException("Unexpected map type: $typeOfSrc")
+        }
+        return Serialization.toProtocol(src)
     }
-    return toProtocol(src);
-  }
 }
--- playwright/src/main/java/com/microsoft/playwright/impl/serialization/ToLowerCaseSerializer.kt
+++ playwright/src/main/java/com/microsoft/playwright/impl/serialization/ToLowerCaseSerializer.kt
@@ -1,15 +1,16 @@
-package com.microsoft.playwright.impl.serialization;
+package com.microsoft.playwright.impl.serialization
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
+import com.google.gson.JsonElement
+import com.google.gson.JsonPrimitive
+import com.google.gson.JsonSerializationContext
+import com.google.gson.JsonSerializer
+import java.lang.reflect.Type
+import java.util.*
 
-import java.lang.reflect.Type;
-
-public class ToLowerCaseSerializer<E extends Enum<E>> implements JsonSerializer<E> {
-  @Override
-  public JsonElement serialize(E src, Type typeOfSrc, JsonSerializationContext context) {
-    return new JsonPrimitive(src.toString().toLowerCase());
-  }
+class ToLowerCaseSerializer<E : Enum<E>> : JsonSerializer<E?>
+{
+    override fun serialize(src: E?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement
+    {
+        return JsonPrimitive(src.toString().lowercase(Locale.getDefault()))
+    }
 }
