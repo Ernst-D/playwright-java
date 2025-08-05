8ddd41d0dae7070e3b9ec5f533733fcd27481497
Ernst-D <ernst.dzerevanko@gmail.com>
2025-08-05 14:28:09 +0400

    comment for HarRecorder

--- playwright/src/main/java/com/microsoft/playwright/impl/HarRecorder.kt
+++ playwright/src/main/java/com/microsoft/playwright/impl/HarRecorder.kt
@@ -3,4 +3,7 @@ package com.microsoft.playwright.impl
 import com.microsoft.playwright.options.HarContentPolicy
 import java.nio.file.Path
 
-public class HarRecorder (@JvmField val path: Path?, @JvmField val contentPolicy: HarContentPolicy?)
+/**
+ * part of BrowserContextImpl
+ */
+class HarRecorder (@JvmField val path: Path?, @JvmField val contentPolicy: HarContentPolicy?)
