95c8aae4d50bf9d0cf976a8e371849b4ad27576d
Ernst-D <ernst.dzerevanko@gmail.com>
2025-08-05 13:46:35 +0400

    refactor ArtifactImpl to kt

--- playwright/src/main/java/com/microsoft/playwright/impl/ArtifactImpl.kt
+++ playwright/src/main/java/com/microsoft/playwright/impl/ArtifactImpl.kt
@@ -13,79 +13,91 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.microsoft.playwright.impl
 
-package com.microsoft.playwright.impl;
+import com.google.gson.JsonObject
+import com.microsoft.playwright.PlaywrightException
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.nio.file.FileSystems
+import java.nio.file.Path
 
-import com.google.gson.JsonObject;
-import com.microsoft.playwright.PlaywrightException;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-
-import static com.microsoft.playwright.impl.Utils.writeToFile;
-
-class ArtifactImpl extends ChannelOwner {
-  public ArtifactImpl(ChannelOwner parent, String type, String guid, JsonObject initializer) {
-    super(parent, type, guid, initializer);
-  }
-
-  public InputStream createReadStream() {
-    JsonObject result = sendMessage("stream").getAsJsonObject();
-    Stream stream = connection.getExistingObject(result.getAsJsonObject("stream").get("guid").getAsString());
-    return stream.stream();
-  }
-
-  byte[] readAllBytes() {
-    final int bufLen = 1024 * 1024;
-    byte[] buf = new byte[bufLen];
-    int readLen;
-    try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream(); InputStream stream = createReadStream()) {
-      while ((readLen = stream.read(buf, 0, bufLen)) != -1) {
-        outputStream.write(buf, 0, readLen);
-      }
-      return outputStream.toByteArray();
-    } catch (IOException e) {
-      throw new PlaywrightException("Failed to read artifact",  e);
-    }
-  }
-
-  public void cancel() {
-    sendMessage("cancel");
-  }
-
-  public void delete() {
-    sendMessage("delete");
-  }
-
-  public String failure() {
-    JsonObject result = sendMessage("failure").getAsJsonObject();
-    if (result.has("error")) {
-      return result.get("error").getAsString();
-    }
-    return null;
-  }
-
-  public Path pathAfterFinished() {
-    if (connection.isRemote) {
-      throw new PlaywrightException("Path is not available when using browserType.connect(). Use download.saveAs() to save a local copy.");
-    }
-    JsonObject json = sendMessage("pathAfterFinished").getAsJsonObject();
-    return FileSystems.getDefault().getPath(json.get("value").getAsString());
-  }
-
-  public void saveAs(Path path) {
-    if (connection.isRemote) {
-      JsonObject jsonObject = sendMessage("saveAsStream").getAsJsonObject();
-      Stream stream = connection.getExistingObject(jsonObject.getAsJsonObject("stream").get("guid").getAsString());
-      writeToFile(stream.stream(), path);
-      return;
+internal class ArtifactImpl(parent: ChannelOwner?, type: String?, guid: String?, initializer: JsonObject?) :
+    ChannelOwner(parent, type, guid, initializer)
+{
+    fun createReadStream(): InputStream
+    {
+        val result = sendMessage("stream")!!.asJsonObject
+        val stream = connection!!.getExistingObject<Stream?>(result.getAsJsonObject("stream").get("guid").asString)
+        return stream!!.stream()
     }
 
-    JsonObject params = new JsonObject();
-    params.addProperty("path", path.toString());
-    sendMessage("saveAs", params);
-  }
+    fun readAllBytes(): ByteArray?
+    {
+        val bufLen = 1024 * 1024
+        val buf = ByteArray(bufLen)
+        var readLen: Int
+        try
+        {
+            ByteArrayOutputStream().use { outputStream ->
+                createReadStream().use { stream ->
+                    while ((stream.read(buf, 0, bufLen).also { readLen = it }) != -1)
+                    {
+                        outputStream.write(buf, 0, readLen)
+                    }
+                    return outputStream.toByteArray()
+                }
+            }
+        } catch (e: IOException)
+        {
+            throw PlaywrightException("Failed to read artifact", e)
+        }
+    }
+
+    fun cancel()
+    {
+        sendMessage("cancel")
+    }
+
+    fun delete()
+    {
+        sendMessage("delete")
+    }
+
+    fun failure(): String?
+    {
+        val result = sendMessage("failure")!!.asJsonObject
+        if (result.has("error"))
+        {
+            return result.get("error").asString
+        }
+        return null
+    }
+
+    fun pathAfterFinished(): Path
+    {
+        if (connection!!.isRemote)
+        {
+            throw PlaywrightException("Path is not available when using browserType.connect(). Use download.saveAs() to save a local copy.")
+        }
+        val json = sendMessage("pathAfterFinished")!!.asJsonObject
+        return FileSystems.getDefault().getPath(json.get("value").asString)
+    }
+
+    fun saveAs(path: Path)
+    {
+        if (connection!!.isRemote)
+        {
+            val jsonObject = sendMessage("saveAsStream")!!.asJsonObject
+            val stream =
+                connection.getExistingObject<Stream?>(jsonObject.getAsJsonObject("stream").get("guid").asString)
+            Utils.writeToFile(stream!!.stream(), path)
+            return
+        }
+
+        val params = JsonObject()
+        params.addProperty("path", path.toString())
+        sendMessage("saveAs", params)
+    }
 }
