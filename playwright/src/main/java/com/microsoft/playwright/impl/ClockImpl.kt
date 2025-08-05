b6152f0e184978936a419b560c6235b1ccc1089a
Ernst-D <ernst.dzerevanko@gmail.com>
2025-08-05 16:43:57 +0400

    migrate ClockImpl.java to kt

--- playwright/src/main/java/com/microsoft/playwright/impl/ClockImpl.kt
+++ playwright/src/main/java/com/microsoft/playwright/impl/ClockImpl.kt
@@ -1,135 +1,152 @@
-package com.microsoft.playwright.impl;
+package com.microsoft.playwright.impl
 
-import com.google.gson.JsonObject;
-import com.microsoft.playwright.Clock;
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.microsoft.playwright.Clock
+import com.microsoft.playwright.Clock.InstallOptions
+import java.util.*
+import java.util.function.Supplier
 
-import java.util.Date;
+internal class ClockImpl(browserContext: BrowserContextImpl) : Clock
+{
+    private val browserContext: ChannelOwner
 
-class ClockImpl implements Clock {
-  private final ChannelOwner browserContext;
-
-  ClockImpl(BrowserContextImpl browserContext) {
-    this.browserContext = browserContext;
-  }
-
-  private void sendMessageWithLogging(String method, JsonObject params) {
-    String capitalizedMethod = method.substring(0, 1).toUpperCase() + method.substring(1);
-    browserContext.withLogging("Clock." + method,
-        () -> browserContext.sendMessage("clock" + capitalizedMethod, params));
-  }
-
-  @Override
-  public void fastForward(long ticks) {
-    JsonObject params = new JsonObject();
-    params.addProperty("ticksNumber", ticks);
-    sendMessageWithLogging("fastForward", params);
-  }
-
-  @Override
-  public void fastForward(String ticks) {
-    JsonObject params = new JsonObject();
-    params.addProperty("ticksString", ticks);
-    sendMessageWithLogging("fastForward", params);
-  }
-
-  @Override
-  public void install(InstallOptions options) {
-    JsonObject params = new JsonObject();
-    if (options != null) {
-      parseTime(options.time, params);
+    init
+    {
+        this.browserContext = browserContext
     }
-    sendMessageWithLogging("install", params);
-  }
 
-  @Override
-  public void runFor(long ticks) {
-    JsonObject params = new JsonObject();
-    params.addProperty("ticksNumber", ticks);
-    sendMessageWithLogging("runFor", params);
-  }
-
-  @Override
-  public void runFor(String ticks) {
-    JsonObject params = new JsonObject();
-    params.addProperty("ticksString", ticks);
-    sendMessageWithLogging("runFor", params);
-  }
-
-  @Override
-  public void pauseAt(long time) {
-    JsonObject params = new JsonObject();
-    params.addProperty("timeNumber", time);
-    sendMessageWithLogging("pauseAt", params);
-  }
-
-  @Override
-  public void pauseAt(String time) {
-    JsonObject params = new JsonObject();
-    params.addProperty("timeString", time);
-    sendMessageWithLogging("pauseAt", params);
-  }
-
-  @Override
-  public void pauseAt(Date time) {
-    JsonObject params = new JsonObject();
-    params.addProperty("timeNumber", time.getTime());
-    sendMessageWithLogging("pauseAt", params);
-  }
-
-  @Override
-  public void resume() {
-    sendMessageWithLogging("resume", new JsonObject());
-  }
-
-  @Override
-  public void setFixedTime(long time) {
-    JsonObject params = new JsonObject();
-    params.addProperty("timeNumber", time);
-    sendMessageWithLogging("setFixedTime", params);
-  }
-
-  @Override
-  public void setFixedTime(String time) {
-    JsonObject params = new JsonObject();
-    params.addProperty("timeString", time);
-    sendMessageWithLogging("setFixedTime", params);
-  }
-
-  @Override
-  public void setFixedTime(Date time) {
-    JsonObject params = new JsonObject();
-    params.addProperty("timeNumber", time.getTime());
-    sendMessageWithLogging("setFixedTime", params);
-  }
-
-  @Override
-  public void setSystemTime(long time) {
-    JsonObject params = new JsonObject();
-    params.addProperty("timeNumber", time);
-    sendMessageWithLogging("setSystemTime", params);
-  }
-
-  @Override
-  public void setSystemTime(String time) {
-    JsonObject params = new JsonObject();
-    params.addProperty("timeString", time);
-    sendMessageWithLogging("setSystemTime", params);
-  }
-
-  @Override
-  public void setSystemTime(Date time) {
-    JsonObject params = new JsonObject();
-    params.addProperty("timeNumber", time.getTime());
-    sendMessageWithLogging("setSystemTime", params);
-  }
-
-  private static void parseTime(Object time, JsonObject params) {
-    if (time instanceof Long) {
-      params.addProperty("timeNumber", (Long) time);
-    } else if (time instanceof Date) {
-      params.addProperty("timeNumber", ((Date) time).getTime());
-    } else if (time instanceof String) {
-      params.addProperty("timeString", (String) time);
+    private fun sendMessageWithLogging(method: String, params: JsonObject?)
+    {
+        val capitalizedMethod = method.substring(0, 1).uppercase(Locale.getDefault()) + method.substring(1)
+        browserContext.withLogging<JsonElement?>(
+            "Clock." + method, Supplier {
+                browserContext.sendMessage(
+                    "clock" + capitalizedMethod, params
+                )
+            })
     }
-  }
+
+    override fun fastForward(ticks: Long)
+    {
+        val params = JsonObject()
+        params.addProperty("ticksNumber", ticks)
+        sendMessageWithLogging("fastForward", params)
+    }
+
+    override fun fastForward(ticks: String?)
+    {
+        val params = JsonObject()
+        params.addProperty("ticksString", ticks)
+        sendMessageWithLogging("fastForward", params)
+    }
+
+    override fun install(options: InstallOptions?)
+    {
+        val params = JsonObject()
+        if (options != null)
+        {
+            parseTime(options.time, params)
+        }
+        sendMessageWithLogging("install", params)
+    }
+
+    override fun runFor(ticks: Long)
+    {
+        val params = JsonObject()
+        params.addProperty("ticksNumber", ticks)
+        sendMessageWithLogging("runFor", params)
+    }
+
+    override fun runFor(ticks: String?)
+    {
+        val params = JsonObject()
+        params.addProperty("ticksString", ticks)
+        sendMessageWithLogging("runFor", params)
+    }
+
+    override fun pauseAt(time: Long)
+    {
+        val params = JsonObject()
+        params.addProperty("timeNumber", time)
+        sendMessageWithLogging("pauseAt", params)
+    }
+
+    override fun pauseAt(time: String?)
+    {
+        val params = JsonObject()
+        params.addProperty("timeString", time)
+        sendMessageWithLogging("pauseAt", params)
+    }
+
+    override fun pauseAt(time: Date)
+    {
+        val params = JsonObject()
+        params.addProperty("timeNumber", time.getTime())
+        sendMessageWithLogging("pauseAt", params)
+    }
+
+    override fun resume()
+    {
+        sendMessageWithLogging("resume", JsonObject())
+    }
+
+    override fun setFixedTime(time: Long)
+    {
+        val params = JsonObject()
+        params.addProperty("timeNumber", time)
+        sendMessageWithLogging("setFixedTime", params)
+    }
+
+    override fun setFixedTime(time: String?)
+    {
+        val params = JsonObject()
+        params.addProperty("timeString", time)
+        sendMessageWithLogging("setFixedTime", params)
+    }
+
+    override fun setFixedTime(time: Date)
+    {
+        val params = JsonObject()
+        params.addProperty("timeNumber", time.getTime())
+        sendMessageWithLogging("setFixedTime", params)
+    }
+
+    override fun setSystemTime(time: Long)
+    {
+        val params = JsonObject()
+        params.addProperty("timeNumber", time)
+        sendMessageWithLogging("setSystemTime", params)
+    }
+
+    override fun setSystemTime(time: String?)
+    {
+        val params = JsonObject()
+        params.addProperty("timeString", time)
+        sendMessageWithLogging("setSystemTime", params)
+    }
+
+    override fun setSystemTime(time: Date)
+    {
+        val params = JsonObject()
+        params.addProperty("timeNumber", time.getTime())
+        sendMessageWithLogging("setSystemTime", params)
+    }
+
+    companion object
+    {
+        private fun parseTime(time: Any?, params: JsonObject)
+        {
+            if (time is Long)
+            {
+                params.addProperty("timeNumber", time)
+            } else if (time is Date)
+            {
+                params.addProperty("timeNumber", time.getTime())
+            } else if (time is String)
+            {
+                params.addProperty("timeString", time)
+            }
+        }
+    }
 }
