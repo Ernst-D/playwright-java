77f6f2f3c25e1df3d75939af9dbde474ff42740e
Ernst-D <ernst.dzerevanko@gmail.com>
2025-08-05 13:58:12 +0400

    move HarRecorder to kt

--- playwright/src/main/java/com/microsoft/playwright/impl/HarRecorder.kt
+++ playwright/src/main/java/com/microsoft/playwright/impl/HarRecorder.kt
@@ -1,15 +1,6 @@
-package com.microsoft.playwright.impl;
+package com.microsoft.playwright.impl
 
-import com.microsoft.playwright.options.HarContentPolicy;
+import com.microsoft.playwright.options.HarContentPolicy
+import java.nio.file.Path
 
-import java.nio.file.Path;
-
-public class HarRecorder {
-  final Path path;
-  final HarContentPolicy contentPolicy;
-
-  HarRecorder(Path har, HarContentPolicy policy) {
-    path = har;
-    contentPolicy = policy;
-  }
-}
+public class HarRecorder (@JvmField val path: Path?, @JvmField val contentPolicy: HarContentPolicy?)
